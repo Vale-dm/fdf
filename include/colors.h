97ca9a1ff424467b92886196b0e7cf3279b58e30
Valeriano Durán <duranmedinavaleriano@gmail.com>
2026-03-24 22:03:19 +0100

    Add files via upload

---
+++ include/colors.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vduran-m <vduran-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 16:59:18 by vduran-m          #+#    #+#             */
+/*   Updated: 2025/06/20 16:59:19 by vduran-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+# define COLORS_H
+
+# define C_WHITE	0xffffff
+# define C_GREY		0x303030
+
+# define C_RED		0xff0000
+# define C_GREEN	0x00ff00
+# define C_BLUE		0x0000ff
+
+# define C_YELLOW	0xffff00
+# define C_CYAN		0x00ffff
+# define C_PINK		0xff00ff
+
+# define C_ORANGY	0xffa500
+# define C_BLUEY	0x0492c2
+
+#endif
